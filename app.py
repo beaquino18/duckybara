9ac4bf9bb249a3ca7fc41ccce3778582ca8f9b36
yanki-bit <149224933+yanki-bit@users.noreply.github.com>
2025-03-08 17:14:23 -0800

    Form not working, need debug in event type section.

--- app.py
+++ app.py
@@ -7,9 +7,18 @@ def index():
     return render_template('index.html', title='Home')
 
 # The route below is for building a form
-@app.route('/form')
+@app.route('/form',  methods=['POST'])
 def form():
     return render_template('form.html')
+    """
+    # below is the logic for the hobbies field where comma separated values are read individually
+    event_type = request.form['event_type']
+    hobbies = request.form['hobbies'].split(',')
+    # Save the data (for simplicity, we'll just print it here)
+    print(f"Event Type: {event_type}, Hobbies: {hobbies}")
+    return 'Form submitted successfully'
+    """
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
